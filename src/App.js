f8b4d3b9d8cfd7f25a977b409f1c52829c202f32
IndraCahya123 <indracahya51@gmail.com>
2021-05-22 13:57:18 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { Spinner } from 'react-bootstrap';
+import Axios from 'axios';
+import Bowser from 'bowser';
+
 import './App.css';
 
 function App() {
+  const [load, setLoad] = useState(false);
+  const [dataIp, setDataIp] = useState({});
+  const [data, setData] = useState(false);
+
+  const apiKey = "01926e9128be70e5edfda61cb3976e1fbf6a0bda3059eb9d50819063";
+
+  const getIpInfo = async () => {
+    setLoad(true);
+    await Axios.get(`https://api.ipdata.co?api-key=01926e9128be70e5edfda61cb3976e1fbf6a0bda3059eb9d50819063`)
+      .then(response => {
+        setLoad(false);
+        setData(true);
+        setDataIp(response.data);
+      });
+  }
+
+  const browserInfo = Bowser.getParser(window.navigator.userAgent);
+
+  console.log(browserInfo.getBrowser());
+
+  const className = {
+    loadData: {
+      style : "btn btn-success disabled"
+    },
+    successLoadData: {
+      style: "btn btn-success"
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button type="button" className={load ? className.loadData.style : className.successLoadData.style} onClick={() => getIpInfo()}>
+        {
+          load ?
+            <>
+              <Spinner animation="border" variant="danger" size="sm" style={{ marginRight: 8 }} />
+              <span>Getting Data</span>
+            </>
+            :
+            <span>Get Information</span>
+        }
+      </button>
+
+      <div style={ data ? { display: 'block' } : {display : 'none'} }>
+        <span>
+          {
+            JSON.stringify(dataIp)
+          }
+        </span>
+      </div>
     </div>
   );
 }
